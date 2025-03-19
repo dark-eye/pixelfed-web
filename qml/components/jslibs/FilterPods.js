a1b46601bca9519d8ca3ec6f3071fdd6c28d78e8
dark-eye <etherpulse@gmail.com>
2025-03-19 09:18:16 +0200

    Issue #11 : Change to use fedidb.org to  get the server list

--- qml/components/jslibs/FilterPods.js
+++ qml/components/jslibs/FilterPods.js
@@ -3,17 +3,18 @@ WorkerScript.onMessage =  function(message) {
 	var list = message.inData;
 	var retList = [];
 	for(var i in list) {
-		if(message.onlyReg && list[i].openSignups != message.onlyReg ) {
+		if(message.onlyReg && list[i].open_registration != message.onlyReg ) {
 			continue;
 		}
-		if(message.searchTerm == "" || list[i].name && list[i].name.match(new RegExp(message.searchTerm,'i'))
-			|| list[i].host && list[i].host.match(new RegExp(message.searchTerm,'i'))
-		|| list[i].countryCode && list[i].countryCode.match(new RegExp(message.searchTerm,'i')))
+		if(message.searchTerm == "" || list[i].domain && list[i].domain.match(new RegExp(message.searchTerm,'i'))
+			|| list[i].location.city && list[i].location.city.match(new RegExp(message.searchTerm,'i'))
+			|| list[i].description && list[i].description.match(new RegExp(message.searchTerm,'i'))
+		|| list[i].location.country && list[i].location.country.match(new RegExp(message.searchTerm,'i')))
 		{
 			retList.push(list[i]);
 		}
 	}
-	 var results =retList;
+	 var results = retList;
 	 WorkerScript.sendMessage({reply: results});
 }
 
